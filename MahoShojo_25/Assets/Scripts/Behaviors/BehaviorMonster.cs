853c56477298d8f48049eb5f7b5be3f16369c393
Noah <16135831+noahm1216@users.noreply.github.com>
2025-10-01 21:13:05 -0700

    setting up demo feedback pt 3
    + main menu logo swapped to new logo
    + environment levels amended to have smoother wall colliders
    + new desktop icon to match improved resolution

--- MahoShojo_25/Assets/Scripts/Behaviors/BehaviorMonster.cs
+++ MahoShojo_25/Assets/Scripts/Behaviors/BehaviorMonster.cs
@@ -256,8 +256,7 @@ public class BehaviorMonster : MonoBehaviour
             else
             {
                 huntOffX = Random.Range(accetpablePlayerOffsetX.x, accetpablePlayerOffsetX.y);
-                huntOffY = Random.Range(accetpablePlayerOffsetY.x, accetpablePlayerOffsetY.y);
-                print($"Random Hunting Range = {huntOffX}, {huntOffY}");
+                huntOffY = Random.Range(accetpablePlayerOffsetY.x, accetpablePlayerOffsetY.y);                
                 gettingIntoPosition = false;
             }
         }
